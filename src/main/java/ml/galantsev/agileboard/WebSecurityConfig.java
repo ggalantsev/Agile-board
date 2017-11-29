afcb0f93ac7369a41db7e3b335f91bbce1e70518
ggalantsev <ggalantsev@gmail.com>
2017-11-29 18:57:07 +0200

    added spring security

---
+++ src/main/java/ml/galantsev/agileboard/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package ml.galantsev.agileboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN") // Allow create and edit only for Admin
+                .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+                .antMatchers("/api/**", "/built/**", "/dashboard/**").access("hasRole('ADMIN') or hasRole('USER')")
+                .antMatchers("/", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard", false)
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("pass").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER");
+    }
+
+}
