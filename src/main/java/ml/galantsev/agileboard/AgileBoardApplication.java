d3f394080fe0f232bd97913085d232fe83d000af
ggalantsev <ggalantsev@gmail.com>
2017-11-30 12:31:53 +0200

    removed db init

--- src/main/java/ml/galantsev/agileboard/AgileBoardApplication.java
+++ src/main/java/ml/galantsev/agileboard/AgileBoardApplication.java
@@ -23,7 +23,7 @@ public class AgileBoardApplication {
 		SpringApplication.run(AgileBoardApplication.class, args);
 	}
 
-	@PostConstruct
+//	@PostConstruct
 	private void init() {
 		mongoTemplate.getDb().dropDatabase();
 		ticketRepo.save(new Ticket("Create Spring Boot app", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", TicketStatus.DONE));
